cd53584019978588686dfa9badc21fb4eac3a8ac
Edward.Guo <edward.guo@baozun.com>
2019-06-25 11:34:28 +0800

    [chore] 更改tslint 语句结尾不需要加分号

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -85,12 +85,11 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import axios from 'axios';
+import { Component, Prop, Vue } from 'vue-property-decorator'
 
 @Component
 export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+  @Prop() private msg!: string
 }
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router/router';
-import store from './types/store';
-import './registerServiceWorker';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router/router'
+import store from './vuex/store'
+import './registerServiceWorker'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app');
+}).$mount('#app')
